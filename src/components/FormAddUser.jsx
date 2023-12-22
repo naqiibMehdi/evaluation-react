10b886893cb48988be6c93b4deb7ecd5b8802363
Mehdi Benamar <mistahbe213@gmail.com>
2023-12-22 16:20:05 +0100

    ajouter le user depuis le formulaire

--- src/components/FormAddUser.jsx
+++ src/components/FormAddUser.jsx
@@ -1,10 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 
-function FormAddUser() {
+function FormAddUser({ setDisplayFormAddUser, validateUser }) {
+  const [addUser, setAddUser] = useState({})
+
+  const handleChangeUse = (e) => {
+    const { name, value } = e.target
+    setAddUser((prev) => ({ ...prev, [name]: value }))
+  }
+
   return (
     <div className="popup-overlay">
       <div className="w-25">
-        <form className="bg-white p-3 rounded h-100 d-flex flex-column gap-4">
+        <form
+          className="bg-white p-3 rounded h-100 d-flex flex-column gap-4"
+          onSubmit={(e) => {
+            e.preventDefault()
+            validateUser(addUser)
+            setDisplayFormAddUser(false)
+          }}
+        >
           <p className="fs-2">Ajouter un utilisateur</p>
           <div className="form-group">
             <label htmlFor="lastname">Nom</label>
@@ -13,6 +27,8 @@ function FormAddUser() {
               className="form-control"
               id="lastname"
               name="lastname"
+              value={addUser.lastname || ""}
+              onChange={handleChangeUse}
             />
           </div>
           <div className="form-group">
@@ -22,19 +38,48 @@ function FormAddUser() {
               className="form-control"
               id="firstname"
               name="firstname"
+              value={addUser.firstname || ""}
+              onChange={handleChangeUse}
             />
           </div>
           <div className="form-group">
             <label htmlFor="age">Age</label>
-            <input type="number" className="form-control" id="age" name="age" />
+            <input
+              type="number"
+              className="form-control"
+              id="age"
+              name="age"
+              value={addUser.age || ""}
+              onChange={handleChangeUse}
+            />
           </div>
-          <select className="form-select" name="genre">
-            <option value="homme">Homme</option>
-            <option value="femme">Femme</option>
+          <select
+            className="form-select"
+            onChange={handleChangeUse}
+            value={addUser.genre || ""}
+            name="genre"
+          >
+            <option value="">-</option>
+            <option value="homme" name="genre">
+              Homme
+            </option>
+            <option value="femme" name="genre">
+              Femme
+            </option>
           </select>
           <div className="d-flex flex-column row-gap-3">
-            <button className="btn btn-primary">Ajouter l'utilisateur</button>
-            <button className="btn btn-secondary">Annuler</button>
+            <button className="btn btn-primary" type="submit">
+              Ajouter l'utilisateur
+            </button>
+            <button
+              className="btn btn-secondary"
+              onClick={(e) => {
+                e.preventDefault()
+                setDisplayFormAddUser(false)
+              }}
+            >
+              Annuler
+            </button>
           </div>
         </form>
       </div>
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,6 +3,7 @@ import man from "../assets/man.png"
 import woman from "../assets/woman.png"
 import User from "./User"
 import FormAddUser from "./FormAddUser"
+import { v4 as uuidv4 } from "uuid"
 
 function Users() {
   const [users, setUsers] = useState([])
@@ -42,20 +43,23 @@ function Users() {
         age: 45,
         genre: "femme",
       },
-      {
-        id: 4,
-        firstname: "carine",
-        lastname: "carre",
-        picture: woman,
-        age: 45,
-        genre: "femme",
-      },
     ])
   }, [])
 
+  const validateUser = (user) => {
+    setUsers([
+      ...users,
+      { id: uuidv4(), picture: user.genre === "homme" ? man : woman, ...user },
+    ])
+  }
   return (
     <>
-      {displayFormAddUser && <FormAddUser />}
+      {displayFormAddUser && (
+        <FormAddUser
+          setDisplayFormAddUser={setDisplayFormAddUser}
+          validateUser={validateUser}
+        />
+      )}
       <h1 className="text-center my-5">Liste des utilisateurs</h1>
       <div className="row w-25 mx-auto mb-5">
         <button
